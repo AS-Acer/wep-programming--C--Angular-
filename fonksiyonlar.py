eca48cb534bc1ad2e411860225f89a622ab7d366
AS-Acer <148333712+AS-Acer@users.noreply.github.com>
2023-12-04 11:19:05 +0300

    Add files via upload

---
+++ fonksiyonlar.py
@@ -0,0 +1,8 @@
+def kredileriListele(): 
+    krediler = ["Hızlı kredi", "Maaaşını halkbanktan ...", "Mutlu emekli","Özel sektör"]
+    for kredi in krediler :
+        print(kredi)
+   
+    print("Krediler Listelendi")
+
+kredileriListele()

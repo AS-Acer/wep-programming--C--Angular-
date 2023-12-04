f5099168dce39f19600aa410d347681b1d8df983
Asacer <148333712+AS-Acer@users.noreply.github.com>
2023-12-04 16:51:31 +0300

    Second Day
    about the basics of c# part-1

---
+++ classIntro/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string adı = "ahmet ";
+            int yas = 20;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdı = "C#";
+            kurs1.KursEğitmeni = "E.D.";
+            kurs1.izlenmeOranı = 68;
+            
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdı = "java";
+            kurs2.KursEğitmeni = "A.S.A.";
+            kurs2.izlenmeOranı = 64;
+            
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdı = "Html";
+            kurs3.KursEğitmeni = "E.D.K.";
+            kurs3.izlenmeOranı = 52;
+
+            //Console.WriteLine(kurs1.KursAdı+" : "+kurs1.KursEğitmeni);
+            //Console.WriteLine(kurs2.KursAdı + " : " + kurs2.KursEğitmeni);
+            //Console.WriteLine(kurs3.KursAdı + " : " + kurs3.KursEğitmeni);
+            //bunları yapmak yerine : 
+            Kurs[] kurslar = new Kurs[] {kurs1,kurs2,kurs3 };
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdı+" : "+ kurs.KursEğitmeni);
+            }
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdı { get; set; }
+
+        public string KursEğitmeni { get; set; }
+
+        public int izlenmeOranı { get; set; }
+
+    }
+}

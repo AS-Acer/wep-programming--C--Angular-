86cc23e268fff455b0c025653c2492386a959352
AS-Acer <148333712+AS-Acer@users.noreply.github.com>
2023-12-04 11:22:12 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,21 @@
+baslik = "ilginizi çekebilir" #değişken tanımladık
+
+#değişkeni yazdırdık 
+print(baslik)
+
+
+#değişken olmadan yazdırdık 
+print("İlginizi çekebilir")
+
+
+#değişkenler ile değişkeni çağırdığımız yerden kullanabiliriz, kendimizi tekrar etmeden kullmamızı sağlar
+
+baslik #string
+vade = 48 #int
+faizOrani = 1.69 #fload
+
+
+
+print(type(vade))
+print(type(baslik))
+print(type(faizOrani))
